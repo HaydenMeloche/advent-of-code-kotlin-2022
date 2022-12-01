8cf550cbef3d82d7392488ca44d7e7217a5e8dd0
Hayden <meloche.hayden@gmail.com>
2022-12-01 15:51:36 -0500

    Completed day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,16 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val listOfLists = input.flatMapIndexed { index, x ->
+        when {
+            index == 0 || index == input.lastIndex -> listOf(index)
+            x.isEmpty() -> listOf(index - 1, index + 1)
+            else -> emptyList()
+        }
+    }.windowed(size = 2, step = 2) { (from, to) -> input.slice(from..to) }
+
+    val partOne = listOfLists.maxOfOrNull { it.sumOf { num -> num.toInt() } }
+    println("part one answer: $partOne")
+
+    val partTwo = listOfLists.map { it.sumOf { num -> num.toInt() } }.sortedDescending().take(3).sum()
+    println("part two answer: $partTwo")
 }
